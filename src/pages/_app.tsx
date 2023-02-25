98113b95136832b1c0caee7e9faaa9b0e50169e9
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-02-24 20:57:36 -0500

    Set up initial project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+const theme = createTheme({});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="favicon-32x32.png" />
+        <title>Huddle landing page with a single introductory section</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
